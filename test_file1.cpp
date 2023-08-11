6619115fa00d4ac94ada342f092eb61be91864c9
YuchenP <yuchenpeng0712@qq.com>
2023-08-11 10:54:17 +0930

    test

--- test_file1.cpp
+++ test_file1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+
+// Function1 
+void Function1(){
+  cout<<"fjqan"<<endl;
+}
 int main(){
   cout<<"Some Modifications occur"<<endl;
+
+
+void fun2()
+{
+  cout << ("nothing happens") << endl;
+}
+int main()
+{
+  cout << "Some Modifications occur" << endl;
   return 0;
 }
